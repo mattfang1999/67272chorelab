77ecb089b4ef2e06f06e7e4bb65962df3ecb0008
Matthew Fang <matthewfang1999@gmail.com>
2021-02-05 12:37:28 -0500

    Finished testing child

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,16 @@
 class Child < ApplicationRecord
+
+    scope :alphabetical, -> { order('last_name, first_name') }
+    scope :active, -> { where(active: true) }
+
+    has_many :chores 
+ 	has_many :tasks, through: :chores
     
+     validates_presence_of :first_name, :last_name 
+
+     def name
+
+        first_name + ' ' + last_name
+
+    end
 end
